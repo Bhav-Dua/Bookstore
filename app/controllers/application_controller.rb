89b26845782fbfae4f54ba9bf3e4690ead38b8f3
kronos <dua_bhavya@outlook.com>
2023-11-20 15:43:29 -0500

    Implement authorization, login, and logout functionality on the backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authorize
+
+    private
+
+    def authorize
+        @current_user = User.find(session[:user_id])
+        render json: { errors: ["Not Authorized"] }, status: :unauthorized unless @current_user
+    end
 end
