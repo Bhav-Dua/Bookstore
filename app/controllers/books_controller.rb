e0ccc858e4e0e49b5cd8df64992083c3700cb4c9
kronos <dua_bhavya@outlook.com>
2023-11-20 16:39:15 -0500

    Implement book index and creation functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+
+    def index
+        render json: Book.all, status: :ok
+    end
+
+    def create
+        book = Book.create!(book_params)
+        render json: book, status: :created
+    end
+
+    private
+
+    def book_params
+        params.permit(:title, :author, :description, :genre, :isbn, :img, :published_year)
+    end
 end
