b18840d89df07e5f06a86e1812bd3ad2555efb80
kronos <dua_bhavya@outlook.com>
2023-11-20 16:45:34 -0500

    Add validations to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
     has_many :reviews
     has_many :users, through: :orders
     has_many :reviewed_users, through: :reviews, source: :user
+
+    validates_presence_of :title, :author, :genre, :isbn
 end
