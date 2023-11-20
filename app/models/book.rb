fb25aae4b8827d599999818db9ad6971727be69d
kronos <dua_bhavya@outlook.com>
2023-11-20 13:59:31 -0500

    Add necessary associations between models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    has_many :orders
+    has_many :reviews
+    has_many :users, through: :orders
+    has_many :reviewed_users, through: :reviews, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :orders
+    has_many :reviews
+    has_many :books, through: :orders
+    has_many :reviewed_books, through: :reviews, source: :book
 end
