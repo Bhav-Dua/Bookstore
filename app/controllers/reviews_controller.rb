52f844b9a8b75e42204d2a64280e3cc40a6cd2d0
kronos <dua_bhavya@outlook.com>
2023-11-22 22:53:51 -0500

    Implement create, update, and destroy actions on controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+    
+    def create
+        review = @current_user.reviews.create!(review_params)
+        render json: review, status: :created
+    end
+
+    def update
+        review = @current_user.reviews.find(params[:id])
+        review.update!(review_params)
+        render json: review, status: :ok
+    end
+
+    def destroy
+        review = @current_user.reviews.find(params[:id])
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def review_params
+        params.permit(:content, :rating, :book_id)
+    end
 end
