48e861d83ab1c0ec3b910f40220938e3d7bd35e3
kronos <dua_bhavya@outlook.com>
2023-11-22 23:18:04 -0500

    Implement order creation in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+
+    def create
+        order = @current_user.orders.create!(order_params)
+        render json: order
+    end
+
+    private
+
+    def order_params
+        params.permit(:book_id)
+    end
 end
