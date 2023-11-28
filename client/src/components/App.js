9cc0eb7e862d23bf586b75b9d3e814762b399dba
kronos <dua_bhavya@outlook.com>
2023-11-28 01:57:37 -0500

    Implement auto-login and retrieval of books from the database

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,27 @@
 import "semantic-ui-css/semantic.min.css";
-import './App.css';
+import "./App.css";
+import { useDispatch } from 'react-redux'
 
 function App() {
-  return (
-    <div className="App">
+  const dispatch = useDispatch();
+  const [books, setBooks] = useState([]);
 
-    </div>
-  );
+  useEffect(() => {
+    fetch("/me")
+      .then(r => {
+        if(r.ok) {
+          dispatch(login(r.json()))
+        }
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch("/books")
+      .then((r) => r.json())
+      .then(setBooks);
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
