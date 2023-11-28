02bb97697f31e864c017d578ef4ce7104cc00076
kronos <dua_bhavya@outlook.com>
2023-11-28 02:35:20 -0500

    Add missing imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,23 @@
-import "semantic-ui-css/semantic.min.css";
-import "./App.css";
-import { useDispatch } from 'react-redux'
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import { useDispatch } from 'react-redux';
+import { login } from '../features/user/userSlice';
+import { useState, useEffect } from 'react';
 
 function App() {
   const dispatch = useDispatch();
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch("/me")
-      .then(r => {
-        if(r.ok) {
-          dispatch(login(r.json()))
-        }
-      })
-  }, [])
+    fetch('/me').then((r) => {
+      if (r.ok) {
+        dispatch(login(r.json()));
+      }
+    });
+  }, []);
 
   useEffect(() => {
-    fetch("/books")
+    fetch('/books')
       .then((r) => r.json())
       .then(setBooks);
   }, []);
--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { logout } from '../features/user/userSlice';
 
 function LogoutButton() {
   const dispatch = useDispatch();
