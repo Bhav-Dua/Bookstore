b395002fd701df871029c9d1fae2bf0ab5190df8
kronos <dua_bhavya@outlook.com>
2023-11-30 17:32:49 -0500

    Implement dependent destroy clause to delete associated data

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-    has_many :orders
-    has_many :reviews
+    has_many :orders, dependent: :destroy
+    has_many :reviews, dependent: :destroy
     has_many :users, through: :orders
 
     validates_presence_of :title, :author, :genre, :isbn
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :orders
-    has_many :reviews
+    has_many :orders, dependent: :destroy
+    has_many :reviews, dependent: :destroy
     has_many :books, through: :orders
 
     validates :username, presence: true, uniqueness: true
