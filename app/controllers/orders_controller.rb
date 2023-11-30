741c08f15fcebafc276e4e9417acb7de5d8ef49c
kronos <dua_bhavya@outlook.com>
2023-11-30 02:22:04 -0500

    Modify controller to prohibit multiple orders for the same book by one user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,12 @@
 class OrdersController < ApplicationController
 
     def create
-        order = @current_user.orders.create!(order_params)
-        render json: order
+        if @current_user.orders.exists?(book_id: order_params[:book_id])
+            render json: { error: 'User already has an order for this book' }, status: :unprocessable_entity
+        else
+            order = @current_user.orders.create!(order_params)
+            render json: order, status: :created
+        end
     end
 
     private
