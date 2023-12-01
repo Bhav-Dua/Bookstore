e8df4234214fef1ccd59e82107d738a86c017d7d
kronos <dua_bhavya@outlook.com>
2023-12-01 15:48:05 -0500

    Fix validations for published_year

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
     has_many :users, through: :orders
 
     validates_presence_of :title, :author, :genre, :isbn, :img, :published_year
+    validates :published_year, numericality: { greater_than_or_equal_to: 1600, less_than_or_equal_to: Date.today.year, message: "Must be between 1600 and the current year" }
 end
