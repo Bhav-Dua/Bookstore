722282192caf4e821349e4a08a0550ca16dd2e03
kronos <dua_bhavya@outlook.com>
2023-11-30 19:16:30 -0500

    Make image in BookCard clickable and link to BookPage

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -4,7 +4,9 @@ function BookCard({ id, title, author, img }) {
   return (
     <div key={id} className='ui card'>
       <div className='image'>
-        <img src={img} alt={title} />
+        <a className='image' href={`/books/${id}`}>
+          <img src={img} alt={title} style={{ maxWidth: '100%', height: 'auto' }} />
+        </a>
       </div>
       <div className='content'>
         <a className='header' href={`/books/${id}`}>
